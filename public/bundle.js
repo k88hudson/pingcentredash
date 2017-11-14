5aaf04ae78ae2ea8c7b8e98d56246b8932f690d9
k88hudson <k88hudson@gmail.com>
2017-11-14 12:41:42 -0500

    use local file

--- public/bundle.js
+++ public/bundle.js
@@ -1646,8 +1646,9 @@ function emoji( country ){
 		+String.fromCodePoint(s + offset);
 }
 
-const DATA_URL = "https://sql.telemetry.mozilla.org/api/queries/49097/results.json?api_key=";
-const TEN_MINUTES = 10 * 60 * 1000;
+const UPDATE_TIME = 5 * 60 * 1000;
+
+const LOCAL_DATA_URL = "http://localhost:8080/paydirt.json";
 
 function getTopFiveCountries(countries) {
   return countries.sort((a, b) => {
@@ -1661,18 +1662,18 @@ function getTopFiveCountries(countries) {
   }).splice(0, 5);
 }
 
-async function getData(apiKey) {
+async function getLocalData() {
   let resp;
   try {
-    resp = await (await fetch(DATA_URL + apiKey)).json();
+    resp = await (await fetch(LOCAL_DATA_URL)).json();
   } catch (e) {
     console.error(e);
     return;
   }
-  const data = resp.query_result.data.rows;
-  const { total: users, count: newtabs } = data[0];
-  const { total: pocket } = data[1];
-  const countries = getTopFiveCountries(data.splice(2).map(item => ({ code: item["?column?1"], count: item.total })));
+  const users = resp[0][2];
+  const newtabs = resp[0][3];
+  const pocket = resp[1][2];
+  const countries = getTopFiveCountries(resp.splice(2).map(item => ({ code: item[1], count: item[2] })));
   return { users, newtabs, pocket, countries };
 }
 
@@ -1716,12 +1717,14 @@ class App extends React.PureComponent {
       _lastRealUserCount: null
     };
     this.updateData = this.updateData.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
   }
   async updateData() {
-    const data = await getData(this.state.apiKey);
+    // const data = await getData(this.state.apiKey);
+    const data = await getLocalData();
 
     if (data) {
+      console.log("Users updated: " + data.users, new Date());
       this.setState(data);
       // let lastUserDiff;
       // const realDiff = this.state._lastRealUserCount && data.users - this.state._lastRealUserCount;
@@ -1739,13 +1742,15 @@ class App extends React.PureComponent {
       //   _lastRealUserCount: data.users,
       //   lastUserDiff
       // }));
+    } else {
+      console.log("problem updating");
     }
   }
   setIntervals() {
     if (!this.inverval) {
-      this.interval = setInterval(this.updateData, TEN_MINUTES);
+      this.interval = setInterval(this.updateData, UPDATE_TIME);
       setInterval(() => {
-        this.setState({ users: this.state.users + 237 });
+        this.setState({ users: this.state.users + 137 });
       }, 7000);
       setInterval(() => {
         this.setState({ newtabs: this.state.newtabs + 238 });
@@ -1755,31 +1760,22 @@ class App extends React.PureComponent {
       }, 8000);
     }
   }
-  async onSubmit(e) {
-    await this.setState({ apiKey: this.state._apiKey });
+  // async onSubmit(e) {
+  //   await this.setState({apiKey: this.state._apiKey});
+  //   this.updateData();
+  //   this.setIntervals();
+  // }
+  async componentDidMount() {
     this.updateData();
     this.setIntervals();
   }
-  async componentDidMount() {
-    const key = localStorage.getItem("API_KEY");
-    if (key) {
-      await this.setState({ _apiKey: key });
-      this.onSubmit();
-    }
-  }
   render() {
-    if (!this.state.apiKey) {
-      return React.createElement(
-        "form",
-        { onSubmit: this.onSubmit },
-        React.createElement("input", { type: "text", value: this.state._apiKey, onChange: e => this.setState({ _apiKey: e.target.value }) }),
-        React.createElement(
-          "button",
-          null,
-          "SUBMIT"
-        )
-      );
-    }
+    // if (!this.state.apiKey) {
+    //   return (<form onSubmit={this.onSubmit}>
+    //     <input type="text" value={this.state._apiKey} onChange={e => this.setState({_apiKey: e.target.value})} />
+    //     <button>SUBMIT</button>
+    //   </form>);
+    // }
     return React.createElement(
       "main",
       null,
--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import emoji from "country-to-emoji-flag";
 
 const DATA_URL = "https://sql.telemetry.mozilla.org/api/queries/49097/results.json?api_key=";
 const DEFAULT_DIFF = 50493;
-const TEN_MINUTES = 10 * 60 * 1000;
+const UPDATE_TIME = 5 * 60 * 1000;
+
+const LOCAL_DATA_URL = "http://localhost:8080/paydirt.json"
 
 function getTopFiveCountries(countries) {
   return countries.sort((a, b) => {
@@ -34,6 +36,22 @@ async function getData(apiKey) {
   return {users, newtabs, pocket, countries};
 }
 
+async function getLocalData() {
+  let resp;
+  try {
+    resp = await (await fetch(LOCAL_DATA_URL)).json();
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+  const users = resp[0][2];
+  const newtabs = resp[0][3];
+  const pocket = resp[1][2];
+  const countries = getTopFiveCountries(resp.splice(2)
+    .map(item => ({code: item[1], count: item[2]})));
+    return {users, newtabs, pocket, countries};
+}
+
 const Panel = props => (<section className={"panel" + (props.primary ? " primary" : "")}>
   {props.children}
 </section>);
@@ -48,7 +66,7 @@ const Counter = props => (<div>
 </div>);
 
 function increment(currentValue, lastDiff, updateFreq) {
-  const amount = lastDiff / (TEN_MINUTES / updateFreq);
+  const amount = lastDiff / (UPDATE_TIME / updateFreq);
   return currentValue + amount;
 }
 
@@ -65,12 +83,14 @@ class App extends React.PureComponent {
       _lastRealUserCount: null
     };
     this.updateData = this.updateData.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
   }
   async updateData() {
-    const data = await getData(this.state.apiKey);
+    // const data = await getData(this.state.apiKey);
+    const data = await getLocalData();
 
     if (data) {
+      console.log("Users updated: " + data.users, new Date());
       this.setState(data);
       // let lastUserDiff;
       // const realDiff = this.state._lastRealUserCount && data.users - this.state._lastRealUserCount;
@@ -88,13 +108,15 @@ class App extends React.PureComponent {
       //   _lastRealUserCount: data.users,
       //   lastUserDiff
       // }));
+    } else {
+      console.log("problem updating");
     }
   }
   setIntervals() {
     if (!this.inverval) {
-      this.interval = setInterval(this.updateData, TEN_MINUTES);
+      this.interval = setInterval(this.updateData, UPDATE_TIME);
       setInterval(() => {
-        this.setState({users: this.state.users + 237});
+        this.setState({users: this.state.users + 137});
       }, 7000);
       setInterval(() => {
         this.setState({newtabs: this.state.newtabs + 238});
@@ -104,25 +126,22 @@ class App extends React.PureComponent {
       }, 8000);
     }
   }
-  async onSubmit(e) {
-    await this.setState({apiKey: this.state._apiKey});
+  // async onSubmit(e) {
+  //   await this.setState({apiKey: this.state._apiKey});
+  //   this.updateData();
+  //   this.setIntervals();
+  // }
+  async componentDidMount() {
     this.updateData();
     this.setIntervals();
   }
-  async componentDidMount() {
-    const key = localStorage.getItem("API_KEY");
-    if (key) {
-      await this.setState({_apiKey: key});
-      this.onSubmit();
-    }
-  }
   render() {
-    if (!this.state.apiKey) {
-      return (<form onSubmit={this.onSubmit}>
-        <input type="text" value={this.state._apiKey} onChange={e => this.setState({_apiKey: e.target.value})} />
-        <button>SUBMIT</button>
-      </form>);
-    }
+    // if (!this.state.apiKey) {
+    //   return (<form onSubmit={this.onSubmit}>
+    //     <input type="text" value={this.state._apiKey} onChange={e => this.setState({_apiKey: e.target.value})} />
+    //     <button>SUBMIT</button>
+    //   </form>);
+    // }
     return (<main>
       <section className="title">
         <div id="logo" />
