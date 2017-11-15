ca378fbfa7821201fdeb9c36992e67f9b805e71e
k88hudson <k88hudson@gmail.com>
2017-11-15 13:39:09 -0500

    incremement option

--- src/main.js
+++ src/main.js
@@ -93,15 +93,17 @@ class App extends React.PureComponent {
   setIntervals() {
     if (!this.inverval) {
       this.interval = setInterval(this.updateData, TEN_MINUTES);
-      setInterval(() => {
-        this.setState({users: this.state.users + 237});
-      }, 7000);
-      setInterval(() => {
-        this.setState({newtabs: this.state.newtabs + 238});
-      }, 2000);
-      setInterval(() => {
-        this.setState({pocket: this.state.pocket + 13});
-      }, 8000);
+      if (this.props.interval) {
+        setInterval(() => {
+          this.setState({users: this.state.users + 237});
+        }, 7000);
+        setInterval(() => {
+          this.setState({newtabs: this.state.newtabs + 238});
+        }, 2000);
+        setInterval(() => {
+          this.setState({pocket: this.state.pocket + 13});
+        }, 8000);
+      }
     }
   }
   async onSubmit(e) {
@@ -152,4 +154,4 @@ class App extends React.PureComponent {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App increment={window.location.hash === "#increment"} />, document.getElementById("root"))
